bda9d73cb54cc12bee91ae7e9127f1b1f136a5e2
Cristobal Hiza <cristobalhiza@gmail.com>
2026-04-06 22:03:14 -0400

    feat(ui): mejorar diseño de secciones, añadir footer y potenciar slider de testimonios con auto-scroll

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+Timber::render( 'templates/404.twig', $context );
